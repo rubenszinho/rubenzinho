fc5df096430ea7722e2dfff8d8775a43f0833b1c
rubenzinho <samuelrubenscontato@gmail.com>
2021-09-07 17:46:07 +0000

    updating css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee080a569714afa6ef57"; }
+/******/ 		__webpack_require__.h = function() { return "12e645094d552081c80d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
